ee74cafababd41d851e1119ee5c0f055ce902ace
liuyadong <liuyadong@lebut.com>
2024-11-18 21:00:14 +0800

    feat: 创建工作流内容

---
+++ apps/web-antd/src/components/nodes/index.ts
@@ -0,0 +1,32 @@
+import type { FlowNode } from '#/components/flow/types/index';
+
+import { shallowRef } from 'vue';
+
+const files: { [key: string]: { default: () => FlowNode } } = import.meta.glob(
+  './*/index.{ts,tsx}',
+  {
+    eager: true,
+  },
+);
+
+const CustomNodes = shallowRef<FlowNode[]>([]);
+for (const i in files) {
+  const [, type] = i.split('/');
+
+  const d = files[i].default();
+
+  if (d.enable !== false) {
+    const configWidth = d.form?.width || '400px';
+    const width = `${Number.parseFloat(configWidth) + 30}px`;
+
+    CustomNodes.value.push({
+      ...d,
+      type,
+      name: `node-${type}`,
+      icon: type,
+      cardWidth: width,
+    });
+  }
+}
+
+export { CustomNodes };
