64320498f0b3985f3ea60ca1df01586c22ce0858
liuyadong <liuyadong@lebut.com>
2024-11-20 11:30:29 +0800

    feat: 创建工作流内容

--- apps/web-antd/src/views/demos/antd/antFlow.vue
+++ apps/web-antd/src/views/demos/antd/antFlow.vue
@@ -1,22 +1,9 @@
 <script lang="ts" setup>
 import { useRouter } from 'vue-router';
 
-import { useTable } from '@cool-vue/crud';
 import { Button } from 'ant-design-vue';
 
 const router = useRouter();
-
-const Table = useTable({
-  columns: [
-    {
-      label: "状态",
-      prop: "status",
-      component: {
-        name: "cl-switch",
-      },
-    },
-  ],
-});
 
 const addFlow = () => {
   router.push({
@@ -29,8 +16,9 @@ const addFlow = () => {
 </script>
 
 <template>
-  <Button type="primary" @click="addFlow">新增</Button>
-  <cl-table ref="Table" />
+  <div>
+    <Button type="primary" @click="addFlow">新增</Button>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
